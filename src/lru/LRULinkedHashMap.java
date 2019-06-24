61a30c51c9951d7269b0894312baf1aa16a2dab8
dmsansan <562404326@qq.com>
2019-06-24 09:04:36 +0800

    添加spring-boot-quartz任务调度管理系统

---
+++ src/lru/LRULinkedHashMap.java
@@ -0,0 +1,98 @@
+package lru;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * LRU 算法：存储空间有限，将存储空间长时间未访问的数据资源在
+ * 存储空间满时，移除
+ *
+ * 使用链表实现LinkedList
+ * 1、将新的数据保存到链表的头部
+ * 2、在访问数据过程中，将访问到的数据放在链表的头部
+ * 3、当存储空间慢的时候，删除链表的尾部元素
+ */
+public class LRULinkedHashMap<K, V> extends LinkedHashMap<K, V> {
+    private final int maxCapacity;
+
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+
+    private final Lock lock = new ReentrantLock();
+
+    public LRULinkedHashMap() {
+        super(maxCapacity, DEFAULT_LOAD_FACTOR, true);
+        this.maxCapacity = maxCapacity;
+    }
+
+    @Override
+    protected boolean removeEldestEntry(java.util.Map.Entry<K, V> eldest) {
+        return size() > maxCapacity;
+    }
+    @Override
+    public boolean containsKey(Object key) {
+        try {
+            lock.lock();
+            return super.containsKey(key);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+
+    @Override
+    public V get(Object key) {
+        try {
+            lock.lock();
+            return super.get(key);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public V put(K key, V value) {
+        try {
+            lock.lock();
+            return super.put(key, value);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public int size() {
+        try {
+            lock.lock();
+            return super.size();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void clear() {
+        try {
+            lock.lock();
+            super.clear();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public Collection<Map.Entry<K, V>> getAll() {
+        try {
+            lock.lock();
+            return new ArrayList<Map.Entry<K, V>>(super.entrySet());
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        LRULinkedHashMap<String, Object> linkedHashMap = new LRULinkedHashMap<>();
+        linkedHashMap.put("1", "A");
+        System.out.println(linkedHashMap.get("1"));
+    }
+}
