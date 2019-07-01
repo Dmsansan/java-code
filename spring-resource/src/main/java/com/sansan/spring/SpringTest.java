f2d67d2f37918c9e87efc8b0cfc6be302b0fb2ef
dmsansan <562404326@qq.com>
2019-07-01 10:28:12 +0800

    新增spring读取xml文件内容代码

---
+++ spring-resource/src/main/java/com/sansan/spring/SpringTest.java
@@ -0,0 +1,15 @@
+package com.sansan.spring;
+
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.core.io.ClassPathResource;
+
+public class SpringTest {
+
+    public static void main(String[] args) {
+        ClassPathResource classPathResource = new ClassPathResource("beans.xml");
+        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
+        reader.loadBeanDefinitions(classPathResource);
+    }
+}
