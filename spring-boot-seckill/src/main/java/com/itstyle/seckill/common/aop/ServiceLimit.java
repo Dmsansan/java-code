aca7bf8896ee2523cf5bb90c17602ec1b68d7475
dmsansan <562404326@qq.com>
2019-07-09 15:39:51 +0800

    添加秒杀系统

---
+++ spring-boot-seckill/src/main/java/com/itstyle/seckill/common/aop/ServiceLimit.java
@@ -0,0 +1,17 @@
+package com.itstyle.seckill.common.aop;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+/**
+ * 自定义注解  限流
+ * 创建者	张志朋
+ * 创建时间	2015年6月3日
+ */
+@Target({ElementType.PARAMETER, ElementType.METHOD})    
+@Retention(RetentionPolicy.RUNTIME)    
+@Documented    
+public  @interface ServiceLimit { 
+	 String description()  default "";
+}
