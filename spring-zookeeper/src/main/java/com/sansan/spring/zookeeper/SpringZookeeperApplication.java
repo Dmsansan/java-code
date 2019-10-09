cf55cd5ea80d15e1c90ff8ecf76c15b2348cf4e6
dmsansan <562404326@qq.com>
2019-10-09 16:52:36 +0800

    添加zookeeper分布式管理中心代码

---
+++ spring-zookeeper/src/main/java/com/sansan/spring/zookeeper/SpringZookeeperApplication.java
@@ -0,0 +1,13 @@
+package com.sansan.spring.zookeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringZookeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringZookeeperApplication.class, args);
+    }
+
+}
