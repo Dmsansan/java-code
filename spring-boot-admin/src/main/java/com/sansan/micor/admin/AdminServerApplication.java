c6b59f9367d885fd3a1fce1df1be43a64e8b90a8
dmsansan <562404326@qq.com>
2020-01-06 15:27:46 +0800

    admin-server结合security使用安全认证机制

--- spring-boot-admin/src/main/java/com/sansan/micor/admin/AdminServerApplication.java
+++ spring-boot-admin/src/main/java/com/sansan/micor/admin/AdminServerApplication.java
@@ -14,36 +14,4 @@ public class AdminServerApplication {
         SpringApplication.run(AdminServerApplication.class, args);
     }
 
-//    @Configuration
-//    public static class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
-//        private final String adminContextPath;
-//
-//        public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
-//            this.adminContextPath = adminServerProperties.getContextPath();
-//        }
-//
-//        @Override
-//        protected void configure(HttpSecurity http) throws Exception {
-//            // @formatter:off
-//            SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
-//            successHandler.setTargetUrlParameter("redirectTo");
-//            successHandler.setDefaultTargetUrl(adminContextPath + "/");
-//
-//            http.authorizeRequests()
-//                    .antMatchers(adminContextPath + "/assets/**").permitAll()
-//                    .antMatchers(adminContextPath + "/login").permitAll()
-//                    .anyRequest().authenticated()
-//                    .and()
-//                    .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
-//                    .logout().logoutUrl(adminContextPath + "/logout").and()
-//                    .httpBasic().and()
-//                    .csrf()
-//                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-//                    .ignoringAntMatchers(
-//                            adminContextPath + "/instances",
-//                            adminContextPath + "/actuator/**"
-//                    );
-//            // @formatter:on
-//        }
-//    }
 }
