84ff9e566e3081d47e9d3ca7a1c5bbaef06bb79a
dmsansan <562404326@qq.com>
2020-05-18 14:23:50 +0800

    添加redis分布式锁实现模块

---
+++ redis-lock/src/test/java/com/sansan/redis/redislock/RedisLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sansan.redis.redislock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- spring-boot/src/main/java/com/sansan/springboot/api/interceptor/RedisSessionInterceptor.java
+++ spring-boot/src/main/java/com/sansan/springboot/api/interceptor/RedisSessionInterceptor.java
@@ -1,6 +1,5 @@
 package com.sansan.springboot.api.interceptor;
 
-import com.alibaba.fastjson.JSON;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
