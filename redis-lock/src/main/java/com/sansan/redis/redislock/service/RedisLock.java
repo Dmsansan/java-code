18c6613107554f8abeeeb2568aa4d277817421a9
dmsansan <562404326@qq.com>
2020-05-19 15:00:29 +0800

    调整RedisLock注释代码

---
+++ redis-lock/src/main/java/com/sansan/redis/redislock/service/RedisLock.java
@@ -0,0 +1,57 @@
+package com.sansan.redis.redislock.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.params.SetParams;
+
+/**
+ * @author siss
+ * @date 2020/5/18  14:49
+ */
+@Service
+public class RedisLock {
+
+    private String lock_key = "redis_lock";
+
+    protected long internalLockLeaseTime = 30000;
+
+    private long timeout = 999999;
+
+    SetParams params = SetParams.setParams().nx().px(internalLockLeaseTime);
+
+    @Autowired
+    JedisPool jedisPool;
+
+    /**
+     * 加锁
+     * @param id
+     * @return
+     */
+    public boolean lock(String id){
+        Jedis jedis = jedisPool.getResource();
+        Long start = System.currentTimeMillis();
+        try{
+            for(;;){
+                //SET命令返回OK ，则证明获取锁成功
+                String lock = jedis.set(lock_key, id, params);
+                if("OK".equals(lock)){
+                    return true;
+                }
+                //否则循环等待，在timeout时间内仍未获取到锁，则获取失败
+                long l = System.currentTimeMillis() - start;
+                if (l>=timeout) {
+                    return false;
+                }
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }finally {
+            jedis.close();
+        }
+    }
+}
