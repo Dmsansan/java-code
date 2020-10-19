2c38b24c8a8e88b289f82a72d8f68e543d695c52
dmsansan <562404326@qq.com>
2020-10-19 10:19:36 +0800

    :rocket:添加物流订单操作控制器

--- mongod-order/src/main/java/com/sansan/order/controller/OrderController.java
+++ mongod-order/src/main/java/com/sansan/order/controller/OrderController.java
@@ -1,9 +1,18 @@
 package com.sansan.order.controller;
 
+import com.sansan.order.pojo.Logistics;
+import com.sansan.order.pojo.Order;
+import com.sansan.order.service.OrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 /**
  * @author siss
@@ -14,5 +23,49 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
     private static final Logger LOGGER = LoggerFactory.getLogger(OrderController.class);
 
+    @Autowired
+    OrderService orderService;
 
+    /**
+     * 添加订单
+     *
+     * @param order
+     * @return
+     */
+    @PostMapping(value = "/addOrder", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String addOrder(Order order)
+    {
+        order.setStatus("发货中");
+        order.setOrderTime(new Date());
+        order.setShipTime(new Date());
+        orderService.addOrder(order);
+        return "添加成功";
+    }
+
+    /**
+     * 更新物流状态
+     *
+     * @param logistics
+     * @return
+     */
+    @PostMapping(value = "/updateOrder", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String updateOrder(Logistics logistics)
+    {
+        logistics.setOperationTime(new Date());
+        orderService.addLogisticsAndUpdateStatus(logistics);
+        return "更新成功";
+    }
+
+    /**
+     * 查询订单信息
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping(value = "getOrderById", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Order getOrderById(int id)
+    {
+        Order order=orderService.getOrderById(id);
+        return  order;
+    }
 }
--- mongod-order/src/main/java/com/sansan/order/service/OrderService.java
+++ mongod-order/src/main/java/com/sansan/order/service/OrderService.java
@@ -1,10 +1,14 @@
 package com.sansan.order.service;
 
+import com.sansan.order.pojo.Logistics;
+import com.sansan.order.pojo.Order;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.Order;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 /**
@@ -13,8 +17,28 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class OrderService {
-    private static final Logger LOGGER = LoggerFactory.getLogger(Order.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderService.class);
 
     @Autowired
     MongoTemplate mongoTemplate;
+
+    public void addOrder(Order order){
+        mongoTemplate.insert(order, "order");
+    }
+
+    public void addLogisticsAndUpdateStatus(Logistics logistics) {
+        String status=logistics.getOperation();
+        Query query = new Query(Criteria.where("_id").is(logistics.getOrderId()));
+        Update update = new Update();
+        // 更新状态
+        update.set("status", status);
+        update.push("logistics",logistics);
+        mongoTemplate.upsert(query, update, Order.class);
+    }
+
+    public Order getOrderById(int id) {
+        Query query = new Query(Criteria.where("_id").is(id));
+        Order order=mongoTemplate.findOne(query, Order.class);
+        return order;
+    }
 }
