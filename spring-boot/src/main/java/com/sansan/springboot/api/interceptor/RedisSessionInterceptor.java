78ecc4366b05c81f491cb30c7d6935debca99384
Sansan.si <sisansan123>
2022-02-12 10:15:03 +0800

    code格式调整

--- spring-boot/src/main/java/com/sansan/springboot/api/interceptor/RedisSessionInterceptor.java
+++ spring-boot/src/main/java/com/sansan/springboot/api/interceptor/RedisSessionInterceptor.java
@@ -8,35 +8,27 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
  * 拦截登录失效的请求
  */
-public class RedisSessionInterceptor implements HandlerInterceptor
-{
+public class RedisSessionInterceptor implements HandlerInterceptor {
     @Autowired
     private StringRedisTemplate redisTemplate;
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception
-    {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         // 无论访问的地址是不是正确的，都进行登录验证，登录成功后的访问再进行分发，404的访问自然会进入到错误控制器中
         String loginUserId = String.valueOf(request.getParameter("loginUserId"));
-        if (!StringUtils.isEmpty(loginUserId))
-        {
-            try
-            {
+        if (!StringUtils.isEmpty(loginUserId)) {
+            try {
                 // 验证当前请求的session是否是已登录的session
                 String loginSessionId = redisTemplate.opsForValue().get("loginUser:" + loginUserId);
-                if (loginSessionId != null && loginSessionId.equals(loginUserId))
-                {
+                if (loginSessionId != null && loginSessionId.equals(loginUserId)) {
                     return true;
                 }
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
@@ -45,30 +37,24 @@ public class RedisSessionInterceptor implements HandlerInterceptor
         return false;
     }
 
-    private void response401(HttpServletResponse response)
-    {
+    private void response401(HttpServletResponse response) {
         response.setCharacterEncoding("UTF-8");
         response.setContentType("application/json; charset=utf-8");
 
-        try
-        {
+        try {
             response.getWriter().print("用户未登录");
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception
-    {
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
 
     }
 
     @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception
-    {
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
 
     }
 }
